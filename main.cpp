1ff64f25c970f62ec40e534f318d3627517915bd
Severin Amann <amann.severin@gmx.de>
2024-12-16 10:36:03 +0100

    Beliebiger Kommentar

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "WeatherAnalyzer.hpp"
 #include "WeatherPresenter.hpp"
 
+// Beliebiger Kommentar
+
 int main() {
     OpenMeteoWeatherService weatherService;  // Create dependency (OpenMeteoWeatherService)
     WeatherAnalyzer weather(weatherService); // Inject dependency into WeatherAnalyzer
